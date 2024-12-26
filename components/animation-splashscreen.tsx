b221e09c7d564d256a3e5c2e1f3224a029735f87
anonymze <anodevfr@gmail.com>
2024-12-27 00:19:17 +0100

    add: prompt ia

--- components/animation-splashscreen.tsx
+++ components/animation-splashscreen.tsx
@@ -1,23 +1,37 @@
-import { useRef } from "react";
-import { View } from "react-native";
 import LottieView from "lottie-react-native";
+import { View } from "react-native";
+import { Link } from "expo-router";
+import React from "react";
+
 
 interface AnimatedScreenProps {
 	setShowAnimation: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function AnimationSplashscreen({ setShowAnimation }: AnimatedScreenProps) {
+	const animationRef = React.useRef<LottieView>(null);
+
+	React.useEffect(() => {
+		// weird bug, i have to set the animation for the next tick 
+		// otherwise the animation is not launching after splashScreen
+		setTimeout(() => {
+			animationRef.current?.play();
+		}, 1);
+	}, []);
+
 	return (
 		<View className="flex-1 items-center justify-center">
+			<Link href="/">debug</Link>
 			<LottieView
-				autoPlay
+				ref={animationRef}
 				loop={false}
+				autoPlay
 				style={{
 					width: 200,
 					height: 200,
 					backgroundColor: "#ffffff",
 				}}
-				source={require("@/resources/cooking.lottie")}
+				source={require("@/resources/cooking2.lottie")}
 				onAnimationFinish={() => setShowAnimation(false)}
 			/>
 		</View>
