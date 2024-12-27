bd875d4d448f69553fa8edbfdb2e11ce21bfde21
anonymze <anodevfr@gmail.com>
2024-12-27 21:25:32 +0100

    .

--- front/app/(recipes)/recipes.tsx
+++ front/app/(recipes)/recipes.tsx
@@ -1,13 +1,61 @@
-import { View } from "react-native";
-import { Link } from "expo-router";
+import { View, TextInput, ScrollView, Text, SafeAreaView } from 'react-native';
+import { generateAPIUrl } from '@/utils/helper';
+import { fetch as expoFetch } from 'expo/fetch';
+import { useChat } from '@ai-sdk/react';
 
 
-interface Props {}
+export default function Page() {
+  const { messages, error, handleInputChange, input, handleSubmit } = useChat({
+    fetch: expoFetch as unknown as typeof globalThis.fetch,
+    api: generateAPIUrl('/api/chat'),
+    onError: error => console.error(error, 'ERROR'),
+  });
 
-export default function Page(props: Props) {
-	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Link href="/frigo">Retour</Link>
-		</View>
-	);
+  if (error) return <Text>{error.message}</Text>;
+
+  return (
+    <SafeAreaView style={{ height: '100%' }}>
+      <View
+        style={{
+          height: '95%',
+          display: 'flex',
+          flexDirection: 'column',
+          paddingHorizontal: 8,
+        }}
+      >
+        <ScrollView style={{ flex: 1 }}>
+          {messages.map(m => (
+            <View key={m.id} style={{ marginVertical: 8 }}>
+              <View>
+                <Text style={{ fontWeight: 700 }}>{m.role}</Text>
+                <Text>{m.content}</Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+
+        <View style={{ marginTop: 8 }}>
+          <TextInput
+            style={{ backgroundColor: 'white', padding: 8 }}
+            placeholder="Say something..."
+            value={input}
+            onChange={e =>
+              handleInputChange({
+                ...e,
+                target: {
+                  ...e.target,
+                  value: e.nativeEvent.text,
+                },
+              } as unknown as React.ChangeEvent<HTMLInputElement>)
+            }
+            onSubmitEditing={e => {
+              handleSubmit(e);
+              e.preventDefault();
+            }}
+            autoFocus={true}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
