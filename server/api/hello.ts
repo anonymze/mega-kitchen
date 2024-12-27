c76ea165b068347915cf1607225d490672efc080
anonymze <anodevfr@gmail.com>
2024-12-27 18:02:21 +0100

    .

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,19 +1,23 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
- 
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
 export const config = {
-  runtime: 'edge',
+	runtime: "edge",
 	// not working on edge function (initial response 25 seconds)
 	// maxDuration: 5,
 };
- 
-export default async function handler(
-	ok: any
-  // request: VercelRequest,
-  // response: VercelResponse,
-) {
-	console.log('ok')
-	console.dir(ok)
+
+export default async function handler(request: Request) {
 	console.log(process.env.VERCEL_REGION);
-  return new Response('Hello from Edge Function!');
-	
+
+	// You can access request properties like this:
+	const { method, url, headers } = request;
+
+	console.log(method, url, headers);
+
+	return new Response("Hello from Edge Function!", {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 }
