6e085131f88a5b17f47816d33cf4398cae2887ba
anonymze <anodevfr@gmail.com>
2024-12-27 17:09:35 +0100

    test

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,14 +1,15 @@
-// import { Request, Response } from '@edge-runtime/primitives'
-
-// export const config = {
-//   runtime: 'edge',
-// }
-
-export function GET(request: Request) {
-	console.log(process.env.VERCEL_REGION);
-  return new Response(JSON.stringify({
-		region: process.env.VERCEL_REGION,
-		runtime: process.env.EDGE_RUNTIME,
-		request: request.url
-	}))
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+ 
+export const config = {
+  runtime: 'edge'
+};
+ 
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+	setTimeout(() => {
+		console.log('hey')
+	}, 1000);
+  return response.status(200).json({ text: 'I am an Edge Function!' });
 }
