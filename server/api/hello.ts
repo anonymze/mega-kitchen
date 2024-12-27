afd5ac14b6566a844eb8e7ad0e4b36eb36cf269b
anonymze <anodevfr@gmail.com>
2024-12-27 16:29:49 +0100

    test

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,8 @@
 // import { Request } from '@edge-runtime/primitives'
 
+export const runtime = 'edge';
+
 export function GET(request: Request) {
+
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
