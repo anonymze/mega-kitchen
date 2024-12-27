932a2ceaaeaf240eff5d8c2e697d95333777e9aa
anonymze <anodevfr@gmail.com>
2024-12-27 17:40:38 +0100

    .

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -9,7 +9,7 @@ export default async function handler(
   // request: VercelRequest,
   // response: VercelResponse,
 ) {
-	await new Promise(resolve => setTimeout(resolve, 5000));
+	await new Promise(resolve => setTimeout(resolve, 40000));
 	console.log(process.env.VERCEL_REGION);
   return new Response('Hello from Edge Function!');
 	
