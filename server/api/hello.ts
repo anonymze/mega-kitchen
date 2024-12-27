a083e6ba5bdd0f314761cf06bb7793310e7d25fc
anonymze <anodevfr@gmail.com>
2024-12-27 16:45:55 +0100

    test

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,12 +1,12 @@
-// import { Request } from '@edge-runtime/primitives'
+import { Request, Response } from '@edge-runtime/primitives'
 
 export const runtime = 'edge';
 
 export function GET(request: Request) {
-  return new Response(JSON.stringify({
-    isEdge: process.env.EDGE_RUNTIME === 'edge',
-    region: process.env.VERCEL_REGION,
-    runtim: process.env.EDGE_RUNTIME,
-		request: request.url
-  }));
+  return new Response().json({
+    isEdge: "bonjour",
+    // region: process.env.VERCEL_REGION,
+    // runtime: process.env.EDGE_RUNTIME,
+		// request: request.url
+  });
 }
