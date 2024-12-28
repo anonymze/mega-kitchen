1acd818f712e631b80fe662d8db651988f2372e6
anonymze <anodevfr@gmail.com>
2024-12-28 02:11:51 +0100

    ok

--- front/components/ui/loader.tsx
+++ front/components/ui/loader.tsx
@@ -1,11 +1,16 @@
-import Animated, { Easing, FadeInUp, useAnimatedStyle, useSharedValue, withRepeat, withTiming, } from "react-native-reanimated";
+import Animated, { Easing, FadeInDown, FadeInUp, useAnimatedStyle, useSharedValue, withRepeat, withTiming, } from "react-native-reanimated";
 import { Loader2Icon } from "lucide-react-native";
 import TailwindConfig from "@/tailwind.config";
+import { cn } from "@/utils/libs/tailwind";
 import { View } from "react-native";
 import React from "react";
 
 
-export default function Loader() {
+interface Props {
+	fadeIn?: "top" | "bottom";
+}
+
+export default function Loader({ fadeIn = "bottom" }: Props) {
 	const rotation = useSharedValue<number>(0);
 
 	React.useEffect(() => {
@@ -15,9 +20,17 @@ export default function Loader() {
 	const animatedStyle = useAnimatedStyle(() => ({
 		transform: [{ rotate: `${rotation.value * 360}deg` }],
 	}));
+	
 	return (
-		<View className="absolute top-10 left-0 right-0 flex-1 items-center">
-			<Animated.View entering={FadeInUp.duration(300).springify()}>
+		<View className={cn(
+			"absolute left-0 right-0 flex-1 items-center",
+			fadeIn === "bottom" ? "bottom-10" : "top-10",
+		)}>
+			<Animated.View
+				entering={
+					fadeIn === "top" ? FadeInUp.duration(300).springify() : FadeInDown.duration(300).springify()
+				}
+			>
 				<Animated.View style={animatedStyle}>
 					<Loader2Icon color={TailwindConfig.theme.colors.primary} size={45} />
 				</Animated.View>
