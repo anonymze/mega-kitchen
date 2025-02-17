2e1dc8e0cbf4484831e18a96ebed9d27a943555a
anonymze <anodevfr@gmail.com>
2025-02-17 13:29:28 +0100

    ini

--- front/components/ui/card.tsx
+++ front/components/ui/card.tsx
@@ -7,8 +7,8 @@ interface Props {
 	alt: string;
 }
 
-// const blurhash =
-// 	"|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+const blurhash =
+	"|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function Card({ source }: Props) {
 	return (
